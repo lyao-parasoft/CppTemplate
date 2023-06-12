b33e730f6a39aa827cd6c7893593511e470005cb
Renee Liu <rliu@parasoft.com>
2023-06-12 10:20:13 +0800

    Publish report to DTP

--- generate-coverage.bat
+++ generate-coverage.bat
@@ -18,4 +18,4 @@ divider_tests.exe
 
 
 cd "../cpptest-coverage/%PROJECT_NAME%"
-cpptestcli -config "builtin://Coverage" -settings "../../../conf/localsettings.properties" -input "%PROJECT_NAME%.clog" -report "../../reports/cpptest-std/ut"
+cpptestcli -config "builtin://Coverage" -settings "../../../conf/localsettings.properties" -input "%PROJECT_NAME%.clog" -report "../../reports/cpptest-std/ut" -publish
