60e86f58639f65b715602bf831c07e4032b9b299
Luffy Yao <lyao@parasoft.com>
2023-08-30 15:41:51 +0800

    generate-coverage.bat edited online with Bitbucket

--- generate-coverage.bat
+++ generate-coverage.bat
@@ -1,20 +1,25 @@
-set "MINGW_HOME=C:/mingw64/bin"
-set "CMKAE_HOME=C:/cmake-3.26.4-windows-x86_64/bin"
-set "CPPTEST_STD_HOME=C:/cpptest_standard-2022.1.0/cpptest"
-set "Path=%CPPTEST_STD_HOME%;%MINGW_HOME%;%CMKAE_HOME%;%Path%"
+rem "Set environment varibles"
+set "MINGW_HOME=C:\alife-tools\MinGW-W64_gcc-8.1.0\MinGW\mingw64"
+set "CMAKE_HOME=C:\alife-tools\cmake"
+set "CPPTEST_STD_HOME=C:\cpptest_standard-2022.1.0\cpptest"
+set "Path=%CPPTEST_STD_HOME%;%CPPTEST_STD_HOME%\bin;%MINGW_HOME%\bin;%CMAKE_HOME%\bin;%Path%"
 
 set "PROJECT_NAME=cpp-project-template"
 set "BUILD_DIR=build"
 
+rem "Compile project"
 rmdir /s /q "%BUILD_DIR%"
 mkdir "%BUILD_DIR%"
 cd "%BUILD_DIR%"
 cmake -DCPPTEST_COVERAGE=ON -DCPPTEST_HOME="%CPPTEST_STD_HOME%" .. -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles"
 make && make install
 
+rem "Run unit test"
 cd test
 divider_tests.exe
 
 
+rem "Generate coverage report"
 cd "../cpptest-coverage/%PROJECT_NAME%"
-cpptestcli -config "builtin://Coverage" -settings "../../../localsettings.properties" -input "%PROJECT_NAME%.clog" -report "../../reports/cpptest-std/ut" -property dtp.url="https://192.168.3.130:8443" -publish
+cpptestcli -config "builtin://Coverage" -settings "../../../localsettings.properties" -input "%PROJECT_NAME%.clog" -report "../../reports/cpptest-std/ut"
+
