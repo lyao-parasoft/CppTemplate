f240baa3b7630b54b7a6a4b03fa77275c7f7de7b
Luffy Yao <lyao@parasoft.com>
2023-10-23 14:42:54 +0800

    main.cpp edited online with Bitbucket

--- test/main.cpp
+++ test/main.cpp
@@ -4,3 +4,8 @@ int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
+
+int main() {
+    std::cout << "this is test" << std::endl;
+    return 0;
+}
